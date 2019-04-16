f99c4216b5fe5b976935326117d7bb4f69d6d356
MJ Krac <github@c40.pl>
2019-04-16 22:12:02 +0100

    validating script

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash -e
+
+# ensuring we're in virtualenv
+if python -c "import sys; exit(1 if hasattr(sys, 'real_prefix') else 0)"
+then
+  echo 'Not in a virtual environment!'
+  exit 1
+fi
+
+black -S --check .
+pycodestyle .
+find . -iname "*.py" | xargs pylint
+pytest .
